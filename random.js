157c5bf30173d04cbb514e7f958c53dca3b978ed
taksh108 <46217981+taksh108@users.noreply.github.com>
2020-08-13 20:50:03 +0530

    Add files via upload

---
+++ random.js
@@ -0,0 +1,37 @@
+
+					/*Random link button- By JavaScript Kit (http://javascriptkit.com) Over 300+ free scripts! This credit MUST stay intact for use*/
+
+					//specify random links below. You can have as many as you want		
+					var randomlinks=new Array()
+					
+					randomlinks[0]="0.html"
+					randomlinks[1]="1.html"
+					randomlinks[2]="2.html"
+					randomlinks[3]="3.html"
+					randomlinks[4]="4.html"
+					randomlinks[5]="5.html"
+					randomlinks[6]="6.html"
+					randomlinks[7]="7.html"
+					randomlinks[8]="8.html"
+					randomlinks[9]="9.html"
+					randomlinks[10]="10.html"
+					randomlinks[11]="11.html"
+					randomlinks[12]="12.html"
+					randomlinks[13]="13.html"
+					randomlinks[14]="14.html"
+					randomlinks[15]="15.html"
+					randomlinks[16]="16.html"
+					randomlinks[17]="17.html"
+					randomlinks[18]="18.html"
+					randomlinks[19]="19.html"
+					randomlinks[20]="20.html"
+					randomlinks[21]="21.html"
+					randomlinks[22]="22.html"
+					randomlinks[23]="23.html"
+					randomlinks[24]="24.html"
+					randomlinks[25]="25.html"
+
+				function randomlink()
+					{
+						window.location=randomlinks[Math.floor(Math.random()*randomlinks.length)]
+					}
